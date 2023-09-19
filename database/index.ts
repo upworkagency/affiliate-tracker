508bb9b8df52f88fe5393ac9333419108c171feb
Lmraza98 <lmraza98@gmail.com>
2023-09-18 21:16:14 -0400

    pair database and Calendly

--- database/index.ts
+++ database/index.ts
@@ -1,16 +1,19 @@
-import { type DB, type  Redirects } from 'kysely-codegen';// this is the Database interface we defined earlier
+import { type DB, type Redirects } from 'kysely-codegen';// this is the Database interface we defined earlier
 import { Pool } from 'pg'
 import { Kysely, PostgresDialect } from 'kysely'
 
+const poolConfig = {
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    database: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    max: Number(process.env.POSTGRES_MAX_POOL),
+};
+  
 const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: 'test',
-    host: 'localhost',
-    user: 'admin',
-    port: 5434,
-    max: 10,
-  })
-})
+    pool: new Pool(poolConfig)
+});
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely 
 // knows your database structure.
@@ -18,7 +21,7 @@ const dialect = new PostgresDialect({
 // to communicate with your database.
 
 export const db = new Kysely<DB>({
-  dialect,
+    dialect,
 })
 
 export type InsertableRedirect = Omit<Redirects, 'id' | 'redirect_timestamp'>;
--- src/edge.ts
+++ src/edge.ts
@@ -1,48 +1,18 @@
 import axios from 'axios';
 import { createRedirectEntry, type InsertableRedirect } from '../database';
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import { getSchedulingUrl } from './calendly';
 
-interface CalendlyResponse {
-  resource: {
-    booking_url: string;
-    owner: string;
-    owner_type: string;
-  };
-}
-
-async function createCalendlyLink(eventTypeUrl: string, bearerToken: string): Promise<string> {
-  try {
-    const response = await axios.post<CalendlyResponse>(
-      'https://api.calendly.com/scheduling_links',
-      {
-        max_event_count: 1,
-        owner: eventTypeUrl,
-        owner_type: 'EventType',
-      },
-      {
-        headers: {
-          'Authorization': `Bearer ${bearerToken}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-//
-    return response.data.resource.booking_url;
-  } catch (error) {
-    console.error('[Error] Failed to create Calendly link:', error);
-    throw error;
-  }
-}
-  
+// eventId: 3b0bfa02-8d78-4865-ad91-405744270db4
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const platform = req.query.platform as string;
     const accountID = req.query.accountID as string;
-    const teamEvent = req.query.team_event as string;  // Retrieve the team_event from the query
+    const eventID = req.query.eventID as string;  // Retrieve the eventID from the query
   
-    console.log(`[Info] Received request for platform: ${platform}, accountID: ${accountID}, team_event: ${teamEvent}`);
+    console.log(`[Info] Received request for platform: ${platform}, accountID: ${accountID}, team_event: ${eventID}`);
   
-    if (!platform || !accountID || !teamEvent) {
-      return res.status(400).json({ error: 'Platform, accountID, and team_event are required.' });
+    if (!platform || !accountID || !eventID) {
+      return res.status(400).json({ error: 'Platform, accountID, and eventID are required.' });
     }
   
     // Record redirection in the database
@@ -53,9 +23,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   
     await createRedirectEntry(redirectData);
     console.log('[Info] Redirect entry created successfully');
-  
-    // Use the teamEvent for generating the eventTypeUrl
-    const eventTypeUrl = `https://api.calendly.com/event_types/${teamEvent}`;
+
     const token = process.env.BEARER_TOKEN;
     if (!token) {
         console.error('Bearer token not provided!');
@@ -63,7 +31,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     }
   
     // Create a single-use Calendly link
-    const calendlyUrl = await createCalendlyLink(eventTypeUrl, token);
+    const calendlyUrl = await getSchedulingUrl(eventID, token);
     console.log('[Info] Generated Calendly URL:', calendlyUrl);
   
     // Redirect user to the generated Calendly link
