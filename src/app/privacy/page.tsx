180d7f34da0a0d68925ac64073c04792a91b440f
Lmraza98 <lmraza98@gmail.com>
2023-10-03 17:24:22 -0400

    added privacy and terms of service page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,61 @@
+export default function Page() {
+    return (
+        <div style={{ maxWidth: '800px', margin: '0 auto', padding: '20px' }}>
+            <h1>Privacy Policy</h1>
+            <p>Last updated: [Date]</p>
+            
+            <h2>1. Introduction</h2>
+            <p>
+                At [Your Company Name], we respect your privacy and are committed to protecting it. 
+                This Privacy Policy explains our policies and practices concerning the collection, use, 
+                and sharing of your personal data or information.
+            </p>
+            
+            <h2>2. Information We Collect</h2>
+            <p>
+                We collect several types of information from and about users of our [website, application, etc.], 
+                including information by which you may be personally identified, such as name, postal address, 
+                email address, or telephone number ("personal information"); and about your internet connection, 
+                the equipment you use to access our [website, application, etc.], and usage details.
+            </p>
+            
+            <h2>3. How We Use Your Information</h2>
+            <p>
+                We use the information we collect about you or that you provide to us, including any personal 
+                data, to provide, maintain, and improve our services, to process transactions, to send you notices 
+                about your account or subscription, and for other purposes with your consent.
+            </p>
+            
+            <h2>4. Disclosure of Your Information</h2>
+            <p>
+                We may disclose aggregated information about our users and information that does not identify 
+                any individual, without restriction. We do not sell or trade your personal information to third parties.
+            </p>
+            
+            <h2>5. Data Security</h2>
+            <p>
+                We have implemented measures designed to secure your personal information from accidental loss 
+                and from unauthorized access, use, alteration, and disclosure.
+            </p>
+            
+            <h2>6. Children's Privacy</h2>
+            <p>
+                Our services are not intended for children under 13 years of age, and we do not knowingly collect 
+                personal information from children under 13.
+            </p>
+            
+            <h2>7. Changes to Our Privacy Policy</h2>
+            <p>
+                It is our policy to post any changes we make to our privacy policy on this page, with a notice that 
+                the privacy policy has been updated on the main page. If we make material changes to how we treat our 
+                users' personal information, we will notify you by email to the email address specified in your account.
+            </p>
+            
+            <h2>8. Contact Information</h2>
+            <p>
+                To ask questions or comment about this privacy policy and our privacy practices, contact us at: 
+                [Your Contact Email].
+            </p>
+        </div>
+    );
+}
